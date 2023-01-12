491a8a3827dc6f45e66832672c2a7f0bc3123581
mahmoudhamdy123 <m.hamdy@spiritude.co>
2023-01-12 12:42:12 +0200

    Rivrpod example

--- lib/feature/rivrpod_example/hello_world_screen.dart
+++ lib/feature/rivrpod_example/hello_world_screen.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// Creating a provider
+final myProvider = Provider((ref) {
+  return MyValue();
+});
+
+//.autoDispose, which will make the provider automatically destroy its state when it is no longer being listened to.
+final myAutoDisposeProvider = StateProvider.autoDispose<int>((ref) => 0);
+//.family, which allows creating a provider from external parameters.
+final myFamilyProvider = Provider.family<String, int>((ref, id) => '$id');
+//A provider can use multiple modifiers at once:
+final userProvider =
+    FutureProvider.autoDispose.family<User, int>((ref, userId) async {
+  return fetchUser(userId);
+});
+
 final hello = Provider<String>((ref) {
   return "Hello World";
+});
+
+//Reading a Provider
+final repositoryProvider = Provider((ref) {
+  return MyValue();
+});
+final provider = Provider((ref) {
+  // use ref to obtain other providers
+  final repository = ref.watch(repositoryProvider);
+
+  return SomeValue(repository);
+});
+//Doing so allows our Counter class to read providers.
+final counterProvider = StateNotifierProvider<Counter, int>((ref) {
+  return Counter(ref);
 });
 
 class HelloWorldScreen extends ConsumerWidget {
@@ -18,5 +48,31 @@ class HelloWorldScreen extends ConsumerWidget {
         ),
       ),
     );
+  }
+}
+
+Future<User> fetchUser(int id) async {
+  return User();
+}
+
+class SomeValue {
+  final MyValue myValue;
+
+  SomeValue(this.myValue);
+}
+
+class User {}
+
+class MyValue {}
+
+class Counter extends StateNotifier<int> {
+  Counter(this.ref) : super(0);
+
+  final Ref ref;
+
+  void increment() {
+    // Counter can use the "ref" to read other providers
+    final repository = ref.read(repositoryProvider);
+    // repository.post('...');
   }
 }
