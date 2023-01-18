1e9fce5d1dd08e0f22cd1c45a4650f86392b629b
mahmoudhamdy123 <m.hamdy@spiritude.co>
2023-01-18 17:14:17 +0200

    Example 5 riverpod

--- lib/feature/rivrpod_example/example_5/home_page_example_5.dart
+++ lib/feature/rivrpod_example/example_5/home_page_example_5.dart
@@ -55,6 +55,28 @@ class HomePageExample5 extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text("Home Page")),
+      body: Consumer(builder: (context, ref, child) {
+        final dataModel = ref.watch(peopleProvider);
+        return ListView.builder(
+          itemCount: dataModel.count,
+          itemBuilder: (context, index) {
+            final person = dataModel.people[index];
+            return ListTile(
+              title: Text(person.displayName),
+            );
+          },
+        );
+      }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Person? person = await createPersonDialog(context);
+          if (person != null) {
+            DataModel dataModel = ref.read(peopleProvider);
+            dataModel.addPerson(person);
+          }
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
